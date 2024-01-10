aac25d1dfd4d0d37e2c89192f85e54e98fdf0c7b
田中　龍 <tanaka@anagram-works.jp>
2024-01-10 21:22:20 +0900

    着手の履歴を保持する

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ function Square({value, onSquareClick}) {
   )
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSuquares] = useState(Array(9).fill(null));
-
+function Board({xIsNext, squares, onPlay}) {
   function handleClick(i) {
     if(squares[i] || calculateWinner(squares)) return;
     const nextSquares = squares.slice();
@@ -20,8 +17,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSuquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares)
   }
 
   const winner = calculateWinner(squares);
@@ -52,6 +48,28 @@ export default function Board() {
       </div>
     </>
   )
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
 }
 
 function calculateWinner(squares) {
